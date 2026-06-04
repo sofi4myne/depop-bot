f04d43aa48d68f5d124635a4154dfc347d652221
Kingston Fitz <kkin6@Kingstons-MacBook-Air.local>
2026-06-03 19:04:09 -0700

    fix zbar import

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ import time
 import requests
 from PIL import Image
 from pdf2image import convert_from_bytes
-import zxingcpp
+from pyzbar.pyzbar import decode as zbar_decode
 from telegram import (
     BotCommand,
     InlineKeyboardButton,
